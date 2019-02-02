cb4c90e4eaa6871d7c84ec3e405aab5b0290037d
Anders Evenrud <andersevenrud@gmail.com>
2019-02-02 19:24:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+<p align="center">
+  <img alt="OS.js Logo" src="https://raw.githubusercontent.com/os-js/gfx/master/logo-big.png" />
+</p>
+
+[OS.js](https://www.os-js.org/) is an [open-source](https://raw.githubusercontent.com/os-js/OS.js/master/LICENSE) desktop implementation for your browser with a fully-fledged window manager, Application APIs, GUI toolkits and filesystem abstraction.
+
+[![Build Status](https://travis-ci.org/os-js/osjs-wireless-tools-provider.svg?branch=master)](https://travis-ci.org/os-js/osjs-wireless-tools-provider)
+[![Support](https://img.shields.io/badge/patreon-support-orange.svg)](https://www.patreon.com/user?u=2978551&ty=h&u=2978551)
+[![Back](https://opencollective.com/osjs/tiers/backer/badge.svg?label=backer&color=brightgreen)](https://opencollective.com/osjs)
+[![Sponsor](https://opencollective.com/osjs/tiers/sponsor/badge.svg?label=sponsor&color=brightgreen)](https://opencollective.com/osjs)
+[![Donate](https://img.shields.io/badge/liberapay-donate-yellowgreen.svg)](https://liberapay.com/os-js/)
+[![Donate](https://img.shields.io/badge/paypal-donate-yellow.svg)](https://paypal.me/andersevenrud)
+[![Community](https://img.shields.io/badge/join-community-green.svg)](https://community.os-js.org/)
+
+# OS.js v3 wireless-tools Service Provider
+
+This provider simply binds https://github.com/bakerface/wireless-tools to the internal API.
+
+## Installation
+
+```bash
+npm install --save --production @osjs/wireless-tools-provider
+```
+
+In your initialization scripts:
+
+```javascript
+// Client index.js file
+import {WirelessToolsServiceProvider} from '@osjs/wireless-tools-provider';
+osjs.register(WirelessToolsServiceProvider);
+
+// Server index.js file
+const {WirelessToolsServiceProvider} = require('@osjs/wireless-tools-provider/src/server.js');
+osjs.register(WirelessToolsServiceProvider);
+```
+
+## API
+
+Simply use `core.make('osjs/wireless-tools').call('namespace', 'method', ...args)` and you will get a `Promise<any, Error>`.
+
+You can also add subscriptions for these calls over websockets so that you get data on a regular interval:
+
+```javascript
+// Subscribe
+core.make('osjs/wireless-tools')
+  .subscribe('ifconfig', 'status')
+  .then(subscription => {
+    // Attach a callback to get data when server pushes it out
+    subscription.bind(data => {
+      console.log(data)
+    });
+
+    // Unsubscribe when you're done
+    subscription.unsubscribe();
+  });
+```
+
+You can see namespaces, method names and the return data in the wireless-tools documentation.
+
+## Features
+
+- Uses 1:1 function signatures (except promise instead of callback)
+- Support for subscriptions over websocket
+- Wifi connection settings via tray icon
+
+## TODO
+
+- [ ] Finish wifi tray icon
+- [ ] Add support for custom intervals on subscriptions
+- [ ] Add support for subscription emit only when data has changed
+
+## Contribution
+
+* **Become a [Patreon](https://www.patreon.com/user?u=2978551&ty=h&u=2978551)**
+* **Support on [Open Collective](https://opencollective.com/osjs)**
+* [Contribution Guide](https://github.com/os-js/OS.js/blob/v3/CONTRIBUTING.md)
+
+## Documentation
+
+See the [Official Manuals](https://manual.os-js.org/v3/) for articles, tutorials and guides.
+
+## Links
+
+* [Official Chat](https://gitter.im/os-js/OS.js)
+* [Community Forums and Announcements](https://community.os-js.org/)
+* [Homepage](https://os-js.org/)
+* [Twitter](https://twitter.com/osjsorg) ([author](https://twitter.com/andersevenrud))
+* [Google+](https://plus.google.com/b/113399210633478618934/113399210633478618934)
+* [Facebook](https://www.facebook.com/os.js.org)
+* [Docker Hub](https://hub.docker.com/u/osjs/)
---
+++ src/client.js
@@ -0,0 +1,193 @@
+import uuid from 'uuid/v4';
+
+class Subscription {
+  constructor(handler) {
+    this.handler = handler;
+    this.callback = () => {};
+    this.id = uuid().replace(/-/g, '');
+  }
+
+  subscribe(ns, method, ...args) {
+    return this.handler.core
+      .request(`/api/wireless-tools/subscribe/${ns}/${method}`, {
+        method: 'POST',
+        body: {id: this.id, args},
+      }, 'json')
+      .then(() => this.handler.add(this));
+  }
+
+  unsubscribe() {
+    return this.handler.core
+      .request(`/api/wireless-tools/unsubscribe/${this.id}`, {
+        method: 'POST'
+      }, 'json')
+      .then(() => this.handler.remove(this));
+  }
+
+  bind(cb) {
+    this.callback = cb;
+  }
+}
+
+class Subscriptions {
+  constructor(core) {
+    this.core = core;
+    this.subscriptions = [];
+    this.started = false;
+  }
+
+  start() {
+    if (this.started) {
+      return;
+    }
+
+    console.debug('@osjs/wireless-tools-provider', 'Subscriptions#start');
+
+    this.started = true;
+
+    this.core.on('osjs/wireless-tools:subscription', (id, data) => {
+      this.emit(id, data);
+    });
+  }
+
+  add(subscription) {
+    console.debug('@osjs/wireless-tools-provider', 'Subscriptions#add', subscription);
+
+    this.subscriptions.push(subscription);
+
+    this.core.ws.send(JSON.stringify({
+      name: 'osjs/wireless-tools:subscription:attach',
+      params: [subscription.id]
+    }));
+  }
+
+  create(ns, method, ...args) {
+    console.debug('@osjs/wireless-tools-provider', 'Subscriptions#create', {
+      ns, method, args
+    });
+
+    const subscription = new Subscription(this);
+
+    return subscription.subscribe(ns, method, ...args)
+      .then(() => {
+        this.subscriptions.push(subscription);
+
+        return subscription;
+      });
+  }
+
+  remove(subscription) {
+    console.debug('@osjs/wireless-tools-provider', 'Subscriptions#remove', subscription);
+
+    const foundIndex = this.subscriptions
+      .findIndex(iter => iter.id === subscription.id);
+
+    if (foundIndex !== -1) {
+      this.subscriptions.splice(foundIndex, 1);
+    }
+  }
+
+  clear() {
+    this.subscription.forEach(iter => iter.unsubscribe());
+    this.subscriptions = [];
+  }
+
+  emit(id, data) {
+    const found = this.subscriptions
+      .find(iter => iter.id === id);
+
+    if (found) {
+      found.callback(data);
+    }
+  }
+}
+
+class WifiTrayIcon {
+  constructor(core) {
+    this.core = core;
+    this.tray = null;
+  }
+
+  destroy() {
+    if (this.tray) {
+      this.tray = this.tray.destroy();
+    }
+  }
+
+  start() {
+    if (this.core.has('osjs/tray') && this.core.has('osjs/theme')) {
+      this.tray = this.core.make('osjs/tray', {
+        icon: this.core.make('osjs/theme').icon('network-wireless'),
+        onclick: ev => this.createWifiContextMenu(ev)
+      });
+    }
+  }
+
+  createWifiContextMenu(ev) {
+    this.core.make('osjs/contextmenu', {
+      position: ev,
+      menu: [{
+        label: 'Connection information',
+        items: [{
+          label: 'foo'
+        }]
+      }, {
+        label: 'Connect to AP'
+      }, {
+        label: 'Mode'
+      }, {
+        separator: true
+      }, {
+        label: 'Disconnect'
+      }]
+    })
+  }
+}
+
+export class WirelessToolsServiceProvider {
+
+  constructor(core, options = {}) {
+    this.core = core;
+    this.options = Object.assign({
+      wifiTray: false
+    }, options);
+
+    this.tray = new WifiTrayIcon(core);
+    this.subscriptions = new Subscriptions(core);
+  }
+
+  destroy() {
+    this.subscriptions.clear();
+    this.tray.destroy();
+  }
+
+  provides() {
+    return [
+      'osjs/wireless-tools',
+    ];
+  }
+
+  init() {
+    this.core.singleton('osjs/wireless-tools', () => ({
+      call: (ns, method, ...args) => this.callApi(ns, method, ...args),
+      subscribe: (ns, method, ...args) => this.subscriptions.create(ns, method, ...args)
+    }));
+
+    return Promise.resolve();
+  }
+
+  start() {
+    this.tray.start();
+    this.subscriptions.start();
+  }
+
+  callApi(ns, method, ...args) {
+    console.debug('@osjs/wireless-tools-provider', 'callApi', ns, method, args);
+
+    return this.core
+      .request(`/api/wireless-tools/${ns}/${method}`, {
+        method: 'POST',
+        body: {args}
+      }, 'json');
+  }
+}
